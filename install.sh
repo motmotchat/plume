e40955b8af2bd5b25bcc45814046caf095795fee
Carl Jackson <carl@avtok.com>
2012-06-30 21:46:59 -0700

    Rename ALL THE THINGS _o/

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#This script bootstraps a new sqlite db for testing
+sqlite3 config.db "CREATE TABLE users(userId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, password TEXT);"
+#sqlite3 config.db "INSERT INTO users(userName,password) values('jeff@motmottest.com','12345');"
+#sqlite3 config.db "INSERT INTO users(userName,password) values('jeff2@motmottest.com','12345');"
+sqlite3 config.db "CREATE TABLE friends(friendId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, friend TEXT, accepted BOOLEAN);"
+mkdir cert
+mkdir ctest
